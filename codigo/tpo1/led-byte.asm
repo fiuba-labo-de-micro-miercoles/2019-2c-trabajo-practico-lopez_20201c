e223af5e4011853df73d6bf84bddbd4a5c91b37a
Santiago <santiago.lopez.497@gmail.com>
2020-05-28 03:55:02 -0300

    Agrego correcciones del tpo1

--- codigo/tpo1/led-byte.asm
+++ codigo/tpo1/led-byte.asm
@@ -1,30 +1,27 @@
 .include "m328pdef.inc"
 
-.dseg
 
-.EQU LED_PORT = 5				; led en PORTB 5
-								; se que el led esta ahi por la hoja de datos
+.cseg 							; todo lo que viene a cont es codigo ejecutable (va en la flash program memory)
+.org 0x0000						; todo lo que viene a cont ponelo a partir de 0x0000
+			jmp		main		; main es una etiqueta
+								; al hacer eso estoy pisando el reset con el jump
 
-.cseg 							; todo lo que viene a cont es codigo ejecutable
-								; (va en la flash program memory)
+.org INT_VECTORS_SIZE			; los perifericos tienen asociada una dir de memoria a partir de la cual se ejecuta codigo especifico
+main:							; INT_VECTOR_SIZE calcula la cantidad de memoria que hay que dejar para esos perifericos
+								; la etiqueta esta definida en el include
+ 			ldi		r23,0xff
+			out		DDRB,r23	; pongo el puerto en modo salida
+		
+led_on:		ldi		r23,0xff
+			out		PORTB,r23 	; enciendo el led 
+	
 
-.org 0x0000						; escribo a continuacion de 0x0000
-			jmp		main		; el programa se va a donde arranca mi codigo
+// inicializo los contadores
+			ldi 	r20,0x00	;  4 * 255
+			ldi 	r21,0x00	; (4 * 255 + 5) * 255
+			ldi		r22,0x00	; (4 * 255 + 5) * 255 + 5) * 64 * 1/f = 1,5s
 
-.org INT_VECTORS_SIZE
-main:
-			ldi		R20,0xff
-			out		DDRB,R20	; pongo en puerto B entero en modo salida
-
-
-led_on:		sbi		PORTB,LED_PORT 	; enciendo el led
-
-// inicializo los contadores		; cuento los ciclos de maquina que quiero
-			ldi 	r20,0x00		;  4 * 255
-			ldi 	r21,0x00		; (4 * 255 + 5) * 255
-			ldi		r22,0x00		; (4 * 255 + 5) * 255 + 5) * 64 * 1/f = 1s
-
-ciclo_encendido:					; chequeo los ciclos de maquina paso a paso
+ciclo_encendido:
 			inc		r20				;((( 1 +
 			cpi		r20,0xff		;    1 +
 			brlo	ciclo_encendido	;    2  )  * 255 +
@@ -37,30 +34,30 @@ ciclo_encendido:					; chequeo los ciclos de maquina paso a paso
 			ldi		r21,0x00		;  ( 1 +
 			inc		r22				;    1 +
 			cpi		r22,0x40		;    1 +
-			brlo	ciclo_encendido ;    2  )) * 64 ; 64 pues voy hasta 0x40
+			brlo	ciclo_encendido ;    2  )) * 64
+			
+			clr		r23
+			out		PORTB,r23		; apago el led
+
+// inicializo los contadores
+			ldi 	r20,0x00	;  4 * 255
+			ldi 	r21,0x00	; (4 * 255 + 5) * 255
+			ldi		r22,0x00	; (4 * 255 + 5) * 255 + 5) * 64 * 1/f = 1,5s
+
+ciclo_apagado:
+			inc		r20				;( 1 +
+			cpi		r20,0xff		;  1 +
+			brlo	ciclo_apagado	;  2  ) * 255
+
+			ldi		r20,0x00		;( 1 +
+			inc		r21				;  1 +
+			cpi		r21,0xff		;  1 +
+			brlo	ciclo_apagado	;  2  )* 255
+
+			ldi		r21,0x00		;( 1 +
+			inc		r22				;  1 +
+			cpi		r22,0x40		;  1 +
+			brlo	ciclo_apagado	;  2  )* 64
 
 
-			cbi		PORTB,5			; apago el led
-
-// inicializo los contadores		; cuento los ciclos de maquina que quiero
-			ldi 	r20,0x00		;  4 * 255
-			ldi 	r21,0x00		; (4 * 255 + 5) * 255
-			ldi		r22,0x00		; (4 * 255 + 5) * 255 + 5) * 64 * 1/f = 1s
-
-ciclo_apagado:						; cheque los ciclos de maquina paso a paso
-			inc		r20				;((( 1 +
-			cpi		r20,0xff		;    1 +
-			brlo	ciclo_apagado	;    2  )  * 255 +
-
-			ldi		r20,0x00		;  ( 1 +
-			inc		r21				;    1 +
-			cpi		r21,0xff		;    1 +
-			brlo	ciclo_apagado	;    2  )) * 255 +
-
-			ldi		r21,0x00		;  ( 1 +
-			inc		r22				;    1 +
-			cpi		r22,0x40		;    1 +
-			brlo	ciclo_apagado 	;    2  )) * 64
-
-
-			RJMP	led_on			; vuelvo a arrancar
+			RJMP	led_on		; vuelvo a arrancar
